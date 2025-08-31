a86812e9bbb510c2c653693e6e70dfc0a4406eb9
Kirill-Bryansk <k.mashnov@yandex.ru>
2025-08-31 22:52:03 +1000

    Ревью 12 спринт

--- src/main/java/ru/yandex/practicum/filmorate/exception/ResponseError.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ResponseError.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.filmorate.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ResponseError {
     private final String error;
     private final String description;
@@ -7,12 +10,5 @@ public class ResponseError {
     public ResponseError(String error, String description) {
         this.error = error;
         this.description = description;
-    }
-    public String getError() {
-        return error;
-    }
-
-    public String getDescription() {
-        return description;
     }
 }
